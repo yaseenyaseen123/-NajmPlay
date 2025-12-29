8976bc624d5ab59744297a0a07014f4ddc0a1d10
yaseenyaseen123 <77697944+yaseenyaseen123@users.noreply.github.com>
2025-12-29 12:06:01 +0000

    نقل ملفات الموقع إلى docs لدعم GitHub Pages بشكل صحيح

---
+++ docs/_next/static/nFDBnYfX61E-Qdv4IRKFg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2fa9e52d53f3bb72.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/admin":["static/chunks/pages/admin-c0a0782378fd5d6f.js"],sortedPages:["/","/_app","/_error","/admin"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
